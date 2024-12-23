2c05a6fdc34b96fae4e064480cecc9a4abef16ff
Laura Dean <lldean18@gmail.com>
2024-12-23 07:12:01 +0000

    writing script to map reads to the references from Mara

--- 05_map_to_morph_genomes/02_map_reads.bash
+++ 05_map_to_morph_genomes/02_map_reads.bash
@@ -13,6 +13,13 @@
 #SBATCH --array=1-23
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
+# set the assembly to be mapped to
+assembly=~/data/Hoverflies/reference_genomes/bombylans_morph/idVolBomb4.fa.gz
+#assembly=~/data/Hoverflies/reference_genomes/plumata_morph/idVolBomb13.fa.gz
+echo "This run is mapping reads to the assembly $assembly"
+
+# Make the config file
+cut -d" " -f1 ${assembly%/*}/chr_sizes.txt > ~/code_and_scripts/config_files/config
 
 
 
