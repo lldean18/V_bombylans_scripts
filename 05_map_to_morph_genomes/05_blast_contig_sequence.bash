f159419c1e82d0d76ab06c959c0b83d79cd5315c
Laura Dean <lldean18@gmail.com>
2025-01-03 15:28:02 +0000

    updated blast contig script to increase time limit and memory because previous run needed more of both. Also hashed out the steps that ran successfully on the first run

--- 05_map_to_morph_genomes/05_blast_contig_sequence.bash
+++ 05_map_to_morph_genomes/05_blast_contig_sequence.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Laura Dean
-# 2/1/25
+# 3/1/25
 # For running on the UoN HPC Ada
 
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=5g
-#SBATCH --time=00:20:00
+#SBATCH --mem=50g
+#SBATCH --time=08:00:00
 #SBATCH --job-name=HF_blast_contig_seq
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
@@ -31,16 +31,16 @@ module load blast-uoneasy/2.14.1-gompi-2023a
 
 
 # extract the contig sequences from the bombylans and plumata references
-zgrep --after-context=1 'ptg000018l' $plumata > $wkdir/plumata_ptg000018l.fasta
-zgrep --after-context=1 'ptg000136l' $bombylans > $wkdir/bombylans_ptg000136l.fasta
-zgrep --after-context=1 'ptg000219l' $bombylans > $wkdir/bombylans_ptg000219l.fasta
+#zgrep --after-context=1 'ptg000018l' $plumata > $wkdir/plumata_ptg000018l.fasta
+#zgrep --after-context=1 'ptg000136l' $bombylans > $wkdir/bombylans_ptg000136l.fasta
+#zgrep --after-context=1 'ptg000219l' $bombylans > $wkdir/bombylans_ptg000219l.fasta
 
 
 
 # make the blast databases
-gunzip -c $reference | makeblastdb -in - -dbtype nucl -title HF_blast_database -out ${reference%.*.*}_blastdb
-gunzip -c $bombylans | makeblastdb -in - -dbtype nucl -title HF_blast_database -out ${bombylans%.*.*}_blastdb
-gunzip -c $plumata | makeblastdb -in - -dbtype nucl -title HF_blast_database -out ${plumata%.*.*}_blastdb
+#gunzip -c $reference | makeblastdb -in - -dbtype nucl -title HF_blast_database -out ${reference%.*.*}_blastdb
+#gunzip -c $bombylans | makeblastdb -in - -dbtype nucl -title HF_blast_database -out ${bombylans%.*.*}_blastdb
+#gunzip -c $plumata | makeblastdb -in - -dbtype nucl -title HF_blast_database -out ${plumata%.*.*}_blastdb
 
 # blast gene sequence against main reference file
 blastn \
